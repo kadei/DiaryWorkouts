007aba4a86d1304beaa320e62a56a5ad977fa782
kadei <serega.kadei@gmail.com>
2015-08-15 13:48:37 +0300

    empty project

---
+++ app/src/main/java/ru/kadei/diaryworkouts/models/EntityTraining.java
@@ -0,0 +1,11 @@
+package ru.kadei.diaryworkouts.models;
+
+/**
+ * Created by kadei on 15.08.15.
+ */
+public class EntityTraining {
+
+    public long id;
+    public String name;
+    public String description;
+}
